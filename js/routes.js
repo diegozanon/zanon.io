2d4dcaad80965bd093994061f38bcbb1a7dbe061
zanon-io <zanon-io@users.noreply.github.com>
2015-09-07 10:59:41 -0300

    Post 3 and adjusting to be more SEO-friendly

--- js/routes.js
+++ js/routes.js
@@ -4,21 +4,24 @@ angular.module('zanonApp').config(['$routeProvider', '$locationProvider',
       
       // partials
       .when('/', {
-        templateUrl: 'partials/posts.html',
+        templateUrl: '/partials/posts.html',
         controller: 'PostsController'
       })	
       .when('/404', {
-        templateUrl: 'partials/404.html',
+        templateUrl: '/partials/404.html',
         controller: '404Controller'
       })		  
 
       // posts
-      .when('/angularjs-with-pretty-urls-removing-the-in-amazon-s3', {
-        templateUrl: 'posts/2015-08-09-angularjs-with-pretty-urls-removing-the-in-amazon-s3.html'
-      })      
-      .when('/angularjs-how-to-create-a-spa-crawlable-and-seo-friendly', {
-        templateUrl: 'posts/2015-08-23-angularjs-how-to-create-a-spa-crawlable-and-seo-friendly.html'
+      .when('/posts/serving-gzipped-files-in-amazon-s3-cloudfront', {
+        templateUrl: '/partials/posts/2015-09-06-serving-gzipped-files-in-amazon-s3-cloudfront.html'
       }) 
+      .when('/posts/angularjs-how-to-create-a-spa-crawlable-and-seo-friendly', {
+        templateUrl: '/partials/posts/2015-08-23-angularjs-how-to-create-a-spa-crawlable-and-seo-friendly.html'
+      }) 
+      .when('/posts/angularjs-with-pretty-urls-removing-the-in-amazon-s3', {
+        templateUrl: '/partials/posts/2015-08-09-angularjs-with-pretty-urls-removing-the-in-amazon-s3.html'
+      })            
 
       // 404
       .otherwise({
