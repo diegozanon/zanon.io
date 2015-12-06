822483a0cbac13c69e071a47fc8a3c92a9c1e545
zanon-io <zanon-io@users.noreply.github.com>
2015-12-06 13:42:42 -0200

    Fixing site templates

---
+++ templates/routes.js
@@ -0,0 +1,24 @@
+angular.module('exampleApp').config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider
+
+      // partials
+      .when('/', {
+        templateUrl: '/partials/posts.html',
+        controller: 'PostsController'
+      })
+      .when('/404', {
+        templateUrl: '/partials/404.html',
+        controller: '404Controller'
+      })
+
+      // posts
+{{posts}}
+      // 404
+      .otherwise({
+        redirectTo: '/404'
+      });
+
+      $locationProvider.html5Mode(true);
+  }
+]);
