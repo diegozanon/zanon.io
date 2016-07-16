b1e6f39c484d229505fecf64bb9365f4333b84a0
zanon-io <zanon-io@users.noreply.github.com>
2016-07-16 13:42:52 -0300

    Adding Twitter button

--- js/directives.js
+++ js/directives.js
@@ -3,11 +3,28 @@ angular.module('zanonApp')
     return {
       restrict: "A",
       link: function(scope, elem, attrs) {
-        scope.$on('$routeChangeSuccess', function (scope, next, current) {          
+        scope.$on('$routeChangeSuccess', function (scope, next, current) {
           var els = angular.element('pre code');
           angular.forEach(els, function(el) {
-            hljs.highlightBlock(el);            
+            hljs.highlightBlock(el);
           });
+        });
+      }
+    };
+  })
+  .directive('shareTwitter', function() {
+    return {
+      link: function(scope, element, attr) {
+        setTimeout(function() {
+          twttr.widgets.createShareButton(
+            attr.url,
+            element[0],
+            {
+              size: attr.size,
+              via: attr.via,
+              text: attr.text
+            }
+          );
         });
       }
     };
